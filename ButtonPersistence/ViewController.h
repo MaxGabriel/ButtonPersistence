02013f1e33c6fc280cf1b902a2bff31b24822f41
Maximilian Tagher <feedback.tagher@gmail.com>
2012-12-02 08:39:54 -0800

    Initial Commit

---
+++ ButtonPersistence/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ButtonPersistence
+//
+//  Created by Maximilian Tagher on 12/2/12.
+//  Copyright (c) 2012 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ButtonPersistence/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ButtonPersistence
+//
+//  Created by Maximilian Tagher on 12/2/12.
+//  Copyright (c) 2012 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
